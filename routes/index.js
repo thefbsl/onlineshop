715907eaa83067a6bae7c248853c62d9aea7087c
thefbsl <96486069+thefbsl@users.noreply.github.com>
2022-04-21 17:46:31 +0600

    Delete index.js

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-let path = require("path");
-router
-    .route("/")
-    .get((req, res) => res.sendFile(path.resolve("index.html")))
-    .post((req, res) => res.send("hello"))
-module.exports = router;
